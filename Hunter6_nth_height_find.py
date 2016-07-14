220d82da547ca099b463a551d0745e3b1665afa0
Hemanth Raj V <raj84325@gmail.com>
2016-07-14 11:55:22 +0530

    Update Hunter6_nth_height_find.py

--- Hunter6_nth_height_find.py
+++ Hunter6_nth_height_find.py
@@ -1,5 +1,5 @@
 a=[int(input()) for i in range(50)]
 a=sorted(a)
-print("The height of 4th tallest is",a[3])
+print("The height of 4th tallest is",a[45])
 print("To find the nth tallest, enter n:")
 print("Height:",a[(int(input()))-1])
